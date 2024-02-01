d9a6cf155b83a878b7a0708340f015d0b87102b9
nibnualdi <nibnualdi@gmail.com>
2024-02-01 18:07:33 +0700

    ref: remove unecessary variable in swiper component

--- components/Swiper/Swiper.tsx
+++ components/Swiper/Swiper.tsx
@@ -47,27 +47,6 @@ const content: SwiperContentType[] = [
   },
 ];
 
-const variantsImg = {
-  enter: (direction: number) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    };
-  },
-};
-
 const variantsDiv = {
   enter: (direction: number) => {
     return {
