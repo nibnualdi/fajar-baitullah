2686d89d725e986eca7ff2fe3ef0d3487fe6cb0e
nibnualdi <nibnualdi@gmail.com>
2024-02-02 11:15:18 +0700

    feat: add link in navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import home from "@/assets/icons/home.json";
 import article from "@/assets/icons/article.json";
 import cellPhone from "@/assets/icons/call-phone.json";
+import Link from "next/link";
 
 const AnimatedIcon = dynamic(() => import("@/components/AnimatedIcon/AnimatedIcon"), {
   ssr: false,
@@ -15,24 +16,25 @@ type navType = {
 };
 
 const nav: navType[] = [
-  { name: "home", icon: home, to: "/home" },
-  { name: "articles", icon: article, to: "/articles" },
+  { name: "home", icon: home, to: "/" },
+  { name: "activity", icon: article, to: "/activity" },
   { name: "contact", icon: cellPhone, to: "/contact" },
 ];
 
 const Navbar = () => {
   return (
     <header className="w-full absolute bg-transparent flex justify-between pl-[69px] pr-[93px] pt-[34px] z-50">
-      <div className="w-[68px] h-[68px] bg-white rounded-full" />
+      <div className="w-[68px] h-[68px] bg-black rounded-full" />
       <div className="text-black flex gap-10">
         {nav.map((item) => (
-          <AnimatedIcon
-            icon={item.icon}
-            className="flex items-center gap-1 font-semibold capitalize cursor-pointer"
-            key={item.name}
-          >
-            <p>{item.name}</p>
-          </AnimatedIcon>
+          <Link href={item.to} key={item.name} className="my-auto">
+            <AnimatedIcon
+              icon={item.icon}
+              className="flex items-center gap-1 font-semibold capitalize cursor-pointer"
+            >
+              <p>{item.name}</p>
+            </AnimatedIcon>
+          </Link>
         ))}
       </div>
     </header>
