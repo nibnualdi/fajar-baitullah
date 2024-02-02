1a8595daccf794b8b668bdc597169685f35296f2
nibnualdi <nibnualdi@gmail.com>
2024-02-02 21:02:13 +0700

    feat: add cards on activity page (have not make the card component)

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,16 +1,99 @@
 import { openSans700, openSans400 } from "@/assets/fonts";
 import { Swiper } from "@/components";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+import flatIcon from "@/assets/icons/flat-arrow.json";
+
+const AnimatedIcon = dynamic(() => import("@/components/AnimatedIcon/AnimatedIcon"), {
+  ssr: false,
+});
+
+const articles = [
+  {
+    id: "1",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "2",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "3",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "4",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "5",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "6",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+  {
+    id: "7",
+    title: "Lorem Ipsum Color Sit Amet.",
+    image:
+      "https://static.republika.co.id/uploads/images/inpicture_slide/aktivitas-warga-saat-shalat-subuh-_150302073711-146.jpg",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.",
+  },
+];
 
 export default function Activity() {
   return (
-    <main className="bg-white h-[1000px]">
-      <div className="w-[1278px] h-[712px] pt-[141px] m-auto">
+    <main className="bg-white py-[141px]">
+      <div className="w-[1278px] h-[712px] m-auto mb-[59px]">
         <Swiper
           containerClassName="max-w-[1278px] w-full h-[712px] rounded-[32px]"
           overlayClassName="backdrop-blur-[32.2px] bg-white/10 max-w-[1245px] w-full h-[300px] rounded-[26px] absolute bottom-[15px] m-auto left-0 right-0 py-[50px] px-[76px] flex flex-col gap-[28px]"
           h1ClassName={`${openSans700.className} text-[48px] max-w-[1106px]`}
           pClassName={`${openSans400.className} text-[20px] max-w-[1106px]`}
         />
+      </div>
+
+      <div className="w-[1278px] m-auto flex flex-wrap gap-[37px]">
+        {articles.map((item) => (
+          <div key={item.id} className="max-w-[401px] text-black">
+            <Image
+              src={item.image}
+              alt={item.id}
+              width={401}
+              height={267}
+              className="rounded-[25px] mb-[25px] w-full h-auto"
+            />
+            <div className="flex justify-between">
+              <h3 className={`mb-[9px] text-[24px] ${openSans700.className} line-clamp-1`}>
+                {item.title}
+              </h3>
+              <div className="w-[13.96px] h-[30px] -rotate-45 overflow-hidden relative">
+                <AnimatedIcon icon={flatIcon} className="absolute top-[5px] left-[-17px]" />
+              </div>
+            </div>
+            <h3 className={`text-[20px] ${openSans400.className} line-clamp-3`}>{item.desc}</h3>
+          </div>
+        ))}
       </div>
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.republika.co.id",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
