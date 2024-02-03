db8b997878efbd9e580944c201e7711f83a0d477
nibnualdi <nibnualdi@gmail.com>
2024-02-03 17:04:49 +0700

    style: clamp text

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -19,7 +19,7 @@ export default function Activity() {
           containerClassName="max-w-[1278px] w-full h-[712px] rounded-[32px]"
           overlayClassName="backdrop-blur-[32.2px] bg-white/10 max-w-[1245px] w-full h-[300px] rounded-[26px] absolute bottom-[15px] m-auto left-0 right-0 py-[50px] px-[76px] flex flex-col gap-[28px]"
           h1ClassName={`${openSans700.className} text-[48px] max-w-[1106px]`}
-          pClassName={`${openSans400.className} text-[20px] max-w-[1106px]`}
+          pClassName={`${openSans400.className} text-[20px] max-w-[1106px] line-clamp-3`}
         />
       </div>
 
