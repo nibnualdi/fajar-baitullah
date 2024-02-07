4a57eae29a77dd2234f941c81365d3f8fdc47d11
nibnualdi <nibnualdi@gmail.com>
2024-02-07 16:41:28 +0700

    style: add animation in detail page

--- app/activity/detail/[id]/page.tsx
+++ app/activity/detail/[id]/page.tsx
@@ -2,9 +2,28 @@
 
 import { openSans400, permanentMarker400 } from "@/assets/fonts";
 import { articles } from "@/constans/dummyData";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { Variants, motion, useScroll, useTransform } from "framer-motion";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
+
+const AnimatedLetters = dynamic(() => import("@/components/AnimatedLetters/AnimatedLetters"), {
+  ssr: false,
+});
+
+const variants: Variants = {
+  animate: {
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 1,
+    },
+  },
+};
+
+const childVariants: Variants = {
+  initial: { y: 20, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+};
 
 const Page = ({ params }: { params: { id: string } }) => {
   const ref = useRef(null);
@@ -19,16 +38,24 @@ const Page = ({ params }: { params: { id: string } }) => {
   }, [scrollYProgress]);
 
   return (
-    <div ref={ref} className="h-[calc(100%+800px)] bg-white text-black">
-      <Image
-        alt="activity-image-detail"
-        src={selectedActivity[0].image}
-        layout="fill"
-        objectFit="cover"
-        className="w-full max-h-[575px] !static"
-      />
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      className="h-[calc(100%+800px)] bg-white text-black"
+    >
+      <motion.div variants={childVariants}>
+        <Image
+          alt="activity-image-detail"
+          src={selectedActivity[0].image}
+          layout="fill"
+          objectFit="cover"
+          className="w-full max-h-[575px] !static"
+        />
+      </motion.div>
       <div className="h-[calc(100%-575px)] flex">
-        <div className="relative h-full max-w-[456px] w-full">
+        <motion.div variants={childVariants} className="relative h-full max-w-[456px] w-full">
           <div className="sticky top-[150px] left-[206px] z-10 h-[62.5px] w-fit flex items-center bg-white">
             <p>{number}%</p>
           </div>
@@ -36,15 +63,18 @@ const Page = ({ params }: { params: { id: string } }) => {
             style={{ scaleY: scrollYProgress }}
             className="bg-black h-full w-[1px] absolute top-[0px] left-[217px] origin-top"
           />
-        </div>
+        </motion.div>
         <div className="max-w-[901px] w-full mt-[10px]">
-          <h1 className={`${permanentMarker400.className} text-[96px] leading-[100px] mb-[32px]`}>
-            {selectedActivity[0].title}
-          </h1>
-          <p className={`${openSans400.className} text-[24px]`}>{selectedActivity[0].desc}</p>
+          <AnimatedLetters
+            letters={selectedActivity[0].title}
+            className={`${permanentMarker400.className} text-[96px] leading-[100px] mb-[32px]`}
+          />
+          <motion.p variants={childVariants} className={`${openSans400.className} text-[24px]`}>
+            {selectedActivity[0].desc}
+          </motion.p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
