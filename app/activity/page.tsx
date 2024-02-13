88a281de2ff86bf22b1d00ab0c7efdedc0d925f8
nibnualdi <nibnualdi@gmail.com>
2024-02-13 19:11:39 +0700

    style: add important to height on swiper in article page

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -9,7 +9,7 @@ export default function Activity() {
       <div className="w-[1278px] h-[712px] m-auto mb-[59px]">
         <Swiper
           containerClassName="max-w-[1278px] w-full h-[712px] rounded-[32px]"
-          overlayClassName="backdrop-blur-[32.2px] bg-white/10 max-w-[1245px] w-full h-[300px] rounded-[26px] absolute bottom-[15px] m-auto left-0 right-0 py-[50px] px-[76px] flex flex-col gap-[28px]"
+          overlayClassName="backdrop-blur-[32.2px] bg-white/10 max-w-[1245px] w-full !h-[300px] rounded-[26px] absolute bottom-[15px] m-auto left-0 right-0 py-[50px] px-[76px] flex flex-col gap-[28px]"
           h1ClassName={`${openSans700.className} text-[48px] max-w-[1106px]`}
           pClassName={`${openSans400.className} text-[20px] max-w-[1106px] line-clamp-3`}
         />
