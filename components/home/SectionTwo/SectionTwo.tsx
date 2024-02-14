0823f086a809b2a422529a468cb7de2dbce4eda0
nibnualdi <nibnualdi@gmail.com>
2024-02-14 14:26:03 +0700

    style: add responsive in swiper homepage for small device

--- components/home/SectionTwo/SectionTwo.tsx
+++ components/home/SectionTwo/SectionTwo.tsx
@@ -6,10 +6,10 @@ const SectionTwo = () => {
   return (
     <section className="h-[722px] relative flex justify-center items-center bg-white">
       <Swiper
-        containerClassName="lg:max-w-[1056px] max-w-[556px] w-full max-h-[487px] h-[487px] rounded-[15px]"
-        overlayClassName="flex flex-col gap-[88px] py-[104px] px-[182px] bg-[rgba(34,34,34,0.50)] top-0 left-0"
-        h1ClassName={`${openSans800.className} text-[64px] text-white max-w-md leading-[55px] uppercase line-clamp-2`}
-        pClassName={`${openSans600.className} text-[20px] text-white max-w-32 line-clamp-3`}
+        containerClassName="lg:max-w-[1056px] md:max-w-[600px] max-w-[300px] w-full max-h-[487px] h-[487px] rounded-[15px]"
+        overlayClassName="flex flex-col gap-[88px] py-[104px] md:px-[182px] px-5 md:text-left text-center bg-[rgba(34,34,34,0.50)] top-0 left-0"
+        h1ClassName={`${openSans800.className} md:text-[64px] text-[40px] text-white max-w-md md:mx-0 mx-auto leading-[55px] uppercase line-clamp-2`}
+        pClassName={`${openSans600.className} text-[20px] text-white max-w-32 md:mx-0 mx-auto line-clamp-3`}
       />
       <div className="w-full h-[722px] flex flex-col justify-end pb-10 gap-[-100px]">
         <ParallaxText baseVelocity={0.3}>Artikel</ParallaxText>
