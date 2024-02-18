dbeaff7eda1aca4fcc432b5f6c56aa80acf306d3
nibnualdi <nibnualdi@gmail.com>
2024-02-18 21:31:53 +0700

    style: fix typing format for the description in the dummy data

--- constans/dummyData.ts
+++ constans/dummyData.ts
@@ -12,13 +12,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -27,13 +23,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -42,13 +34,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -57,13 +45,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -72,13 +56,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -87,13 +67,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
   {
@@ -102,13 +78,9 @@ export const articles: articleType[] = [
     image:
       "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
     desc: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. 
-    Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, 
-    nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, 
-    sit amet bibendum justo molestie nec. Donec porttitor dignissim leo eget sollicitudin. 
-
-    Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, 
-    justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac pretium turpis. Vivamus tincidunt enim dapibus urna ultrices imperdiet. Integer viverra, felis sed mollis aliquet, nisl lorem imperdiet erat, in malesuada urna mauris ut nunc. Vestibulum tristique enim nisl, sit amet bibendum justo molestie nec. 
+    
+    Donec porttitor dignissim leo eget sollicitudin. Donec at interdum tortor. Pellentesque vel eleifend nibh. Curabitur imperdiet, lectus in mollis placerat, justo ligula venenatis odio, sit amet mattis dui lorem at nisi. Duis sit amet velit tortor.
     `,
   },
 ];
