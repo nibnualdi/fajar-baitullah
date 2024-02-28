ddc13904002895d92df79a8ede36d1d962bbd240
nibnualdi <nibnualdi@gmail.com>
2024-02-28 13:39:46 +0700

    fix: warning in svg code. It needs to be changed to camelcase

--- assets/icons/admin/ArticleIcon.tsx
+++ assets/icons/admin/ArticleIcon.tsx
@@ -10,8 +10,8 @@ const ArticleIcon = () => {
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         stroke-width="2"
         d="M15 8h2m-2 4h2m0 4H7m0-8v4h4V8H7zM5 20h14a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2z"
       />
