9f93d642f7776e7c5a17411440d77f9e1f488204
nibnualdi <nibnualdi@gmail.com>
2024-05-17 19:51:23 +0700

    fix: add articles dummy to swiper component, it was missing a property

--- app/(user)/(defaultLayout)/activity/page.tsx
+++ app/(user)/(defaultLayout)/activity/page.tsx
@@ -13,6 +13,7 @@ export default function Activity() {
     <main className="bg-white py-[141px]">
       <div className="relative mid-xl:max-w-[1278px] mid-lg:max-w-[840px] max-w-[401px] w-full h-[712px] m-auto mb-[59px]">
         <Swiper
+          articles={articles}
           containerClassName="mid-xl:max-w-[1278px] mid-lg:max-w-[840px] max-w-[401px] w-full h-[712px] rounded-[32px]"
           overlayClassName="backdrop-blur-[32.2px] bg-white/10 max-w-[1245px] w-full !h-[300px] rounded-[26px] absolute bottom-[15px] m-auto left-0 right-0 py-[50px] px-[76px] flex flex-col lg:gap-[28px] gap-5"
           h1ClassName={`${openSans700.className} mid-lg:text-[48px] text-[20px] max-w-[1106px] lg:leading-0 mid-lg:leading-10 leading-0 mid-lg:line-clamp-none line-clamp-3`}
