2614a496337f6a5a78445adec60e44300607def1
nibnualdi <nibnualdi@gmail.com>
2024-05-24 06:42:51 +0700

    feat: slicing the other article in about page

--- components/OverlayAbout/OverlayAbout.tsx
+++ components/OverlayAbout/OverlayAbout.tsx
@@ -4,9 +4,10 @@ import { openSans400, roboto400, roboto700 } from "@/assets/fonts";
 import { toggleDevelopmentAboutPageInView } from "@/lib/features/util/utilSlice";
 import { useAppDispatch } from "@/lib/hooks";
 import { useInView, motion } from "framer-motion";
+import Image from "next/image";
 import React, { useEffect, useRef } from "react";
 
-const content: { title: string; desc: string }[] = [
+const content: { title: string; desc?: string; image?: string; overlay?: boolean }[] = [
   {
     title: "Tentang Kami",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quae iste. Odit, obcaecati dicta facere delectus ipsam rerum sed eum officia expedita quae quisquam dignissimos nam corporis aperiam cupiditate minus! loh",
@@ -18,6 +19,18 @@ const content: { title: string; desc: string }[] = [
   {
     title: "Kontak Kami",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, quae iste. Odit, obcaecati dicta facere delectus ipsam rerum sed eum officia expedita quae quisquam dignissimos nam corporis aperiam cupiditate minus! loh",
+  },
+  {
+    title: "Struktur Organisasi",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
+    overlay: true,
+  },
+  {
+    title: "Lorem Ipsum",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
+    overlay: true,
   },
 ];
 
@@ -33,16 +46,21 @@ const OverlayAbout = () => {
   return (
     <section className="flex pointer-events-none bg-transparent relative" style={{ zIndex: 10 }}>
       <div className="flex flex-col">
-        {content.map((e, index) => (
-          <div
-            className="max-w-[442px] h-screen ml-[10vw] flex flex-col gap-[30px] justify-center pointer-events-auto"
-            id={e.title}
-            key={`${e.title} ${index}`}
-          >
-            <h1 className={`text-dark-green text-[36px] ${roboto700.className}`}>{e.title}</h1>
-            <p className={`text-black text-[20px] ${roboto400.className}`}>{e.desc}</p>
-          </div>
-        ))}
+        {content.map((e, index) => {
+          return (
+            !e.overlay && (
+              <div
+                className="max-w-[442px] h-screen ml-[10vw] flex flex-col gap-[30px] justify-center pointer-events-auto"
+                id={e.title}
+                key={`${e.title} ${index}`}
+              >
+                <h1 className={`text-dark-green text-[36px] ${roboto700.className}`}>{e.title}</h1>
+                <p className={`text-black text-[20px] ${roboto400.className}`}>{e.desc}</p>
+              </div>
+            )
+          );
+        })}
+
         <div
           className="w-screen h-screen relative select-none grid grid-cols-2 grid-rows-2 place-content-around place-items-center gap-32 cursor-grab"
           id="development-mosque"
@@ -94,6 +112,31 @@ const OverlayAbout = () => {
             </div>
           </motion.div>
         </div>
+
+        {content.map((e, index) => {
+          return (
+            e.overlay && (
+              <div className="bg-white-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10">
+                <div
+                  className={`w-screen h-screen p-10 text-dark-green text-[36px] text-center ${roboto700.className} flex flex-col justify-center items-center gap-3 opacity-90`}
+                >
+                  <h1>{e.title}</h1>
+                  {e.image && (
+                    <Image
+                      alt={e.title}
+                      src={e.image}
+                      width={0}
+                      height={0}
+                      sizes="100%"
+                      style={{ width: "100%", height: "100%" }}
+                      className="h-full w-full object-cover rounded-lg p-3"
+                    />
+                  )}
+                </div>
+              </div>
+            )
+          );
+        })}
       </div>
     </section>
   );
