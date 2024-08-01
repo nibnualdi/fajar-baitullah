4d488cbd3adb206388389efbc86adce50079d5d2
nibnualdi <nibnualdi@gmail.com>
2024-08-01 12:25:01 +0700

    set user data to redux

--- app/admin/auth/login/page.tsx
+++ app/admin/auth/login/page.tsx
@@ -7,6 +7,8 @@ import React, { useEffect } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import { jwtFunc } from "@/lib/utils/jwtDecode";
+import { useAppDispatch } from "@/lib/hooks";
+import { setUser } from "@/lib/features/userSlice";
 
 const Form = dynamic(() => import("@/components/Form/Form"), {
   ssr: false,
@@ -14,6 +16,7 @@ const Form = dynamic(() => import("@/components/Form/Form"), {
 
 const Login = () => {
   const router = useRouter();
+  const dispatch = useAppDispatch();
   const token = Cookies.get("session_token") as string;
   const inputs: formInputs = [
     {
@@ -38,6 +41,7 @@ const Login = () => {
       try {
         const payload = await jwtFunc({ token });
         console.log(payload);
+        dispatch(setUser(payload));
         router.push("/admin/dashboard");
       } catch (err) {
         console.log(err);
@@ -48,13 +52,23 @@ const Login = () => {
   const handleSubmit = async (inputState: InputState, e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const { token } = await FetchAPI({
-      endpoint: "/api/user/login",
-      method: "POST",
-      body: JSON.stringify(inputState),
-    });
-    Cookies.set("session_token", token);
-    router.push("/admin/dashboard");
+    try {
+      // login
+      const { token } = await FetchAPI({
+        endpoint: "/api/user/login",
+        method: "POST",
+        body: JSON.stringify(inputState),
+      });
+      Cookies.set("session_token", token);
+
+      // set data user to redux
+      const payload = await jwtFunc({ token });
+      dispatch(setUser(payload));
+
+      router.push("/admin/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className="bg-white text-dark-green flex h-screen flex-col items-center justify-center">
