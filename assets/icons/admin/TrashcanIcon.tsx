2bd6d914495e2c7431fc7787dc939ff691ed60ea
nibnualdi <nibnualdi@gmail.com>
2024-08-29 08:21:42 +0700

    fix: add trash icon

---
+++ assets/icons/admin/TrashcanIcon.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TrashcanIcon = () => {
+  return (
+    <svg
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke="#CCCCCC"
+        stroke-width="0.9600000000000002"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M3 6H21M5 6V20C5 21.1046 5.89543 22 7 22H17C18.1046 22 19 21.1046 19 20V6M8 6V4C8 2.89543 8.89543 2 10 2H14C15.1046 2 16 2.89543 16 4V6"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+        <path d="M14 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M10 11V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+      </g>
+    </svg>
+  );
+};
+
+export default TrashcanIcon;
