b404e3a37a644f4a3c9c9d029c0ce4fcd866f37a
nibnualdi <nibnualdi@gmail.com>
2024-11-04 10:49:01 +0700

    feat: make a function for formating a date

---
+++ lib/utils/formattedDate.ts
@@ -0,0 +1,8 @@
+export const formattedDate = (date: Date) => {
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+
+  const formattedDate = year + "-" + month + "-" + day;
+  return formattedDate;
+};
